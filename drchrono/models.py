5c2a2239363e7305c34ac9fec8df85f4d732771d
Nico-ehs <nico.ehs05@gmail.com>
2019-05-09 13:03:37 -0400

    env setup

--- drchrono/models.py
+++ drchrono/models.py
@@ -1,5 +1,20 @@
 from django.db import models
 
 # Add your models here
+class Doctor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
 
+class Paitent(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
 
+class Appointment(models.Model):
+    doctor=models.ForeignKey(Doctor)
+    paitent=models.ForeignKey(Paitent)
+    description = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    checked_in = models.DateTimeField(blank=True)
+    time_seen = models.DateTimeField(blank=True)
